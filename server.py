7dc2a1a04584e8107420781639448a60b4e3b854
ally2211 <amendieta.va@gmail.com>
2024-08-05 02:03:55 -0500

    stripe product payment for stripe product enabled

--- server.py
+++ server.py
@@ -6,28 +6,32 @@ Stripe Sample.
 Python 3.6 or newer required.
 """
 import os
-from flask import Flask, redirect, request
+from flask import Flask, redirect, request, url_for
 
 import stripe
-# This test secret API key is a placeholder. Don't include personal details in requests with this key.
-# To see your test secret API key embedded in code samples, sign in to your Stripe account.
-# You can also find your test secret API key at https://dashboard.stripe.com/test/apikeys.
-stripe.api_key = 'sk_test_4eC39HqLyjWDarjtT1zdp7dc'
+# stripe.api_key = 'sk_test_4eC39HqLyjWDarjtT1zdp7dc'
+
 
 app = Flask(__name__,
             static_url_path='',
             static_folder='public')
 
-YOUR_DOMAIN = 'http://localhost:4242'
+app.config['STRIPE_PUBLIC_KEY'] = 'pk_test_51J4u0IEYLRwiTz0PHTGslxBoGFtRVzT6906tDUGwgdkQBYKe8KVunyayFs9yfB2uOZIqmrpUaFx746DKQmMYq1UN00sUFrBBmd';
+app.config['STRIPE_PRIVATE_KEY'] = 'sk_test_51J4u0IEYLRwiTz0P9ijaLBqhNc5kfx1yuijAUgQ4w4qCspcRIdf53U2BuK3lcOsl5Ts63izbH1ZnkX72YZoMOeZB00Y60M84jn';
+stripe.api_key = app.config['STRIPE_PRIVATE_KEY'];
+
+YOUR_DOMAIN = 'http://localhost:4242/'
+
 
 @app.route('/create-checkout-session', methods=['POST'])
 def create_checkout_session():
     try:
         checkout_session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
             line_items=[
                 {
                     # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                    'price': '{{PRICE_ID}}',
+                    'price': 'price_1PkKCGEYLRwiTz0PY7kj6Oza',
                     'quantity': 1,
                 },
             ],
