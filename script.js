f2d7472dd40cd059f92bac0cf154583441cac0b7
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-08-13 00:30:21 -0500

    back to top

--- script.js
+++ script.js
@@ -9,6 +9,20 @@ $(document).ready(function () {
     $("#loader").hide();
   }
 
+  const backToTopButton = document.getElementById("back-to-top");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+      backToTopButton.style.display = "block";
+    } else {
+      backToTopButton.style.display = "none";
+    }
+  });
+
+  backToTopButton.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+
   var splide = new Splide(".splide", {
     type: "loop",
     autoplay: true,
